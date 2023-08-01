747384ee93f12add675b500f3347ab6f57a66811
Seif <seifaziz10@gmail.com>
2023-08-01 11:35:34 -0700

    Get proxy server working

--- docker/server/index.js
+++ docker/server/index.js
@@ -1,33 +1,32 @@
 const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require("dotenv").config();
 const app = express();
 const port = 3000; // 4444 for local dev, 3000 for Docker
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: true }));
 app.use("/", express.static("build"));
 
-app.post("/v1/query", async (req, res) => {
-  console.log("req.body", req.body);
-  try {
-    const response = await axios.post(`https://${process.env.endpoint}/v1/query`, req.body, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "customer-id": process.env.customer_id,
-        "x-api-key": process.env.api_key,
-        "grpc-timeout": "60S",
-      },
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error sending search request:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+const proxyOptions = {
+  target: `https://${process.env.endpoint}`,
+  changeOrigin: true,
+  pathRewrite: { '^/v1/query': '/v1/query' },
+  onProxyReq: (proxyReq, req) => {
+    proxyReq.setHeader("Content-Type", "application/json");
+    proxyReq.setHeader("Accept", "application/json");
+    proxyReq.setHeader("customer-id", process.env.customer_id);
+    proxyReq.setHeader("x-api-key", process.env.api_key);
+    proxyReq.setHeader("grpc-timeout", "60S");
+
+    if (req.body) {
+      const bodyData = JSON.stringify(req.body);
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      proxyReq.write(bodyData);
+    }
+  },
+};
+
+app.use('/v1/query', createProxyMiddleware(proxyOptions));
 
 app.get("/", function (req, res) {
   res.render("build/index.html");
@@ -139,4 +138,6 @@ app.post("/config", (req, res) => {
   });
 });
 
-app.listen(port, () => { });
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- server/index.js
+++ server/index.js
@@ -1,38 +1,35 @@
 const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require("dotenv").config();
 const app = express();
 const port = 4444; // 4444 for local dev, 3000 for Docker
 
-// app.use(express.json());
+app.use(express.json());
 app.use("/", express.static("build"));
 
 app.get("/", function (req, res) {
   res.render("build/index.html");
 });
 
-app.post("/v1/query", async function (req, res) {
-  console.log(2 + 2);
-  // const headers = {
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Accept: "application/json",
-  //     "customer-id": process.env.customer_id,
-  //     "x-api-key": process.env.api_key,
-  //     "grpc-timeout": "60S",
-  //   }
-  // };
-  console.log(req.body);
-  // try {
-  //   const response = await axios.post(`https://${process.env.endpoint}/v1/query`, req.body, headers);
-  //   res.json(response.data);
-  // } catch (error) {
-  //   console.error("Error sending search request:", error);
-  //   res.status(500).json({ error: "Internal server error" });
-  // }
-});
+const proxyOptions = {
+  target: `https://${process.env.endpoint}`,
+  changeOrigin: true,
+  pathRewrite: { '^/v1/query': '/v1/query' },
+  onProxyReq: (proxyReq, req) => {
+    proxyReq.setHeader("Content-Type", "application/json");
+    proxyReq.setHeader("Accept", "application/json");
+    proxyReq.setHeader("customer-id", process.env.customer_id);
+    proxyReq.setHeader("x-api-key", process.env.api_key);
+    proxyReq.setHeader("grpc-timeout", "60S");
+
+    if (req.body) {
+      const bodyData = JSON.stringify(req.body);
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      proxyReq.write(bodyData);
+    }
+  },
+};
+app.use('/v1/query', createProxyMiddleware(proxyOptions));
 
 app.post("/config", (req, res) => {
   const {
