8a9c0f185994b990de552481f4d56598bbec93e7
CJ Cenizal <cj@cenizal.com>
2023-10-23 11:56:59 -0700

    Render summary as Markdown.

--- src/ui/utils/citations/extractCitations.test.ts
+++ src/ui/utils/citations/extractCitations.test.ts
@@ -7,40 +7,43 @@ describe("extractCitations", () => {
 
     expect(extractCitations(summary)).toEqual([
       { text: "", references: ["1"] },
-      { text: "Beginning of summary.", references: ["2"] },
+      { text: " Beginning of summary. ", references: ["2"] },
       { text: "", references: ["3"] },
       {
-        text: "Multiple at beginning of sentence, and before comma",
-        references: ["4"]
+        text: " Multiple at beginning of sentence, and before comma ",
+        references: ["4"],
       },
-      { text: ", single at middle", references: ["5"] },
+      { text: ", single at middle ", references: ["5"] },
       {
-        text: "of sentence. At end of sentence",
-        references: ["6"]
+        text: " of sentence. At end of sentence ",
+        references: ["6"],
       },
-      { text: "." }
+      { text: "." },
     ]);
   });
 
   test("extracts citation at the end of the summary", () => {
     const summary = "End of summary. [1]";
-    expect(extractCitations(summary)).toEqual([{ text: "End of summary.", references: ["1"] }]);
+    expect(extractCitations(summary)).toEqual([
+      { text: "End of summary. ", references: ["1"] },
+    ]);
   });
 
   test("extracts multiple comma-delimited citations", () => {
-    const summary = "Two citations [1, 2] and seven citations [1, 2, 3, 4, 5, 6, 7] and without spaces [1,2,3].";
+    const summary =
+      "Two citations [1, 2] and seven citations [1, 2, 3, 4, 5, 6, 7] and without spaces [1,2,3].";
 
     expect(extractCitations(summary)).toEqual([
-      { text: "Two citations", references: ["1", "2"] },
+      { text: "Two citations ", references: ["1", "2"] },
       {
-        text: "and seven citations",
-        references: ["1", "2", "3", "4", "5", "6", "7"]
+        text: " and seven citations ",
+        references: ["1", "2", "3", "4", "5", "6", "7"],
       },
       {
-        text: "and without spaces",
-        references: ["1", "2", "3"]
+        text: " and without spaces ",
+        references: ["1", "2", "3"],
       },
-      { text: "." }
+      { text: "." },
     ]);
   });
 });
