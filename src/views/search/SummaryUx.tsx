b1102757ef2840dfe00779fb5705428ef4c25958
Ofer Mendelevitch <ofermend@gmail.com>
2023-11-19 20:34:39 -0800

    initial

--- src/views/search/SummaryUx.tsx
+++ src/views/search/SummaryUx.tsx
@@ -10,18 +10,21 @@ import { ProgressReport } from "./progressReport/ProgressReport";
 import { SummaryCitation } from "./summary/SummaryCitation";
 import { DeserializedSearchResult } from "./types";
 
+import { HfInference } from '@huggingface/inference'
+import { useEffect, useState } from "react";
+
 export const SummaryUx = () => {
   const {
     isSearching,
     searchResults,
     isSummarizing,
+    summaryNumResults,
     summarizationResponse,
     searchResultsRef,
     selectedSearchResultPosition,
   } = useSearchContext();
 
   const rawSummary = summarizationResponse?.summary[0]?.text;
-
   const unorderedSummary = sanitizeCitations(rawSummary);
 
   let summary = "";
@@ -37,6 +40,52 @@ export const SummaryUx = () => {
     }
   }
 
+  // compute the HEM score
+  const summaryWithoutCitations = rawSummary?.replace(/\[[0-9]+\]/g, "");
+
+  const API_URL = "https://api-inference.huggingface.co/models/vectara/hallucination_evaluation_model";
+  const API_KEY = "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"; /// TBD: how to read API key from config?
+  const inference = new HfInference(API_KEY)
+  const hem = inference.endpoint(API_URL)
+
+  async function getHEMScore(inputText: string): Promise<any> {
+    const responseData = await hem.textClassification({ inputs: inputText })
+//    console.log("Query = ", inputText)
+//    console.log("Response = ", JSON.stringify(responseData, null, 2))
+    const hemScore = responseData[0].score;
+    return Math.round(hemScore * 100) / 100; // round to 2 digits
+  }
+
+  async function getMaxScore() {
+    const scorePromises = (searchResults?.slice(0, summaryNumResults) || []).map((result, index) => {
+      const { snippet: { pre, post, text } } = result;
+      const query = [pre, text, post, "[SEP]", summaryWithoutCitations].join(" ");
+      const score = getHEMScore(query);
+      return score
+    });
+  
+    try {
+      const scores = await Promise.all(scorePromises);
+      console.log("Scores: ", scores)
+      return Math.max(...scores);
+    } catch (error) {
+      console.error("An error occurred while processing scores: ", error);
+      return -1; // Optional: early return in case of error
+    }
+  }
+
+  const [maxScore, setMaxScore] = useState<number>(-1);
+  useEffect(() => {
+    if ((summaryWithoutCitations !== undefined && typeof summaryWithoutCitations === 'string' && summaryWithoutCitations.length > 0) && 
+        (Array.isArray(searchResults) && searchResults.length >= summaryNumResults)) {
+      getMaxScore().then(score => {
+        setMaxScore(score);
+      }).catch(error => {
+        console.error("Error getting max score: ", error);
+      });
+    }
+  }, [searchResults]);
+
   return (
     <>
       <ProgressReport isSearching={isSearching} isSummarizing={isSummarizing} />
@@ -47,7 +96,7 @@ export const SummaryUx = () => {
 
           <VuiTitle size="xs">
             <h2>
-              <strong>Summary</strong>
+              <strong> {maxScore > 0 ? `Summary (${maxScore})` : 'Summary'} </strong>
             </h2>
           </VuiTitle>
 
@@ -78,4 +127,4 @@ export const SummaryUx = () => {
       )}
     </>
   );
-};
+}
