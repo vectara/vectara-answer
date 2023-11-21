ef6a3883e2c69d2702f2aeb9d5a28782f6c5ea4e
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2023-11-21 20:51:50 +0530

    feat: Added necessary changes in `run.sh` file

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,3 @@
-
 if [ $# -lt 2 ]; then
   echo "Missing arguments."
   echo "Usage: $0 <config-folder> <secrets-profile>"
@@ -10,24 +9,19 @@ if [ ! -d "$1" ]; then
   exit 2
 fi
 
-# build docker container
-npm install & npm run build
-docker build -f docker/Dockerfile . --tag=vectara-answer:latest
-
-# run the docker with the right configuration
+# Generate .env file for docker
 python3 docker/prepare_config.py $1 $2
-docker rm vanswer -f 2> /dev/null
 
 if [[ $1 == /* ]] || [[ $1 == ~* ]]; then
     correct_path=$(eval echo $1)
 else
     correct_path="$PWD/$1"
 fi
-docker run --platform=linux/amd64 -d  -v $correct_path/queries.json:/usr/src/app/build/queries.json  --env-file .env --name vanswer -p 127.0.0.1:80:3000/tcp vectara-answer
+docker compose up -d
 if [ $? -eq 0 ]; then
-  echo "Success! Application is running at http://localhost:80.\nTo shut it down use: docker container stop vanswer."
+  echo "Success! Application is running at http://localhost:3000.\nTo shut it down use: docker compose down"
   sleep 5
-  open http://localhost
+  open http://localhost:3000
 else
   echo "Container failed to start."
 fi
