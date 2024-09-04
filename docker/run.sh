0aeea901d03f8ca0f9a5249d60a2b74b1722688e
Saptarshi Sarkar <saptarshi.programmer@gmail.com>
2024-09-04 18:21:39 +0530

    Fix: Added shell code to check if Docker is running in background and exit accordingly (#168)
    fix(Docker): Added shell code to check if Docker is running in background

--- docker/run.sh
+++ docker/run.sh
@@ -18,6 +18,12 @@ if [ $# -eq 2 ]; then
   fi
 fi
 
+# Check if docker is running
+if ! docker info > /dev/null 2>&1; then
+  echo "Docker is not running. Please start Docker and try again."
+  exit 1
+fi
+
 # build the image
 docker build -f docker/Dockerfile . --tag=vectara-answer:latest
 if [ $? -ne 0 ]; then
